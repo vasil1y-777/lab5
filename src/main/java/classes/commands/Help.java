ab4b936f6e0916418d47330c7cc0d99101225ee2
Nikulin Vasiliy <nikulin.vasily.777@ya.ru>
2023-02-26 17:30:11 +0300

    Added NamedCommand abstract class (#7)

--- src/main/java/classes/commands/Help.java
+++ src/main/java/classes/commands/Help.java
@@ -1,17 +1,23 @@
 package classes.commands;
 
-import interfaces.Command;
+import classes.NamedCommand;
+import interfaces.Commandable;
 import org.reflections.Reflections;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Set;
 
-public class Help implements Command {
+public class Help extends NamedCommand implements Commandable {
     @Override
-    public void execute() {
+    public String getInfo() {
+        return getName() + "\t-\tвывести справку по доступным командам";
+    }
+
+    @Override
+    public void execute(String... args) {
         Reflections reflections = new Reflections("classes.commands");
-        Set<Class<? extends Command>> allCommands = reflections.getSubTypesOf(Command.class);
-        for (Class<? extends Command> command : allCommands) {
+        Set<Class<? extends Commandable>> allCommands = reflections.getSubTypesOf(Commandable.class);
+        for (Class<? extends Commandable> command : allCommands) {
             try {
                 System.out.println(command.getDeclaredConstructor().newInstance().getInfo());
             } catch (IllegalAccessException | InstantiationException | InvocationTargetException |
@@ -21,13 +27,5 @@ public class Help implements Command {
         }
     }
 
-    @Override
-    public String getName() {
-        return "help";
-    }
 
-    @Override
-    public String getInfo() {
-        return getName() + "\t-\tвывести справку по доступным командам";
-    }
 }
