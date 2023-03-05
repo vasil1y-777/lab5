9b4e5864a0dc030eb81a973e5b53e5c579af741a
GreedlyCore <naganariselves@gmail.com>
2023-03-05 13:56:45 +0300

    add output for clear command

--- src/main/java/classes/commands/Clear.java
+++ src/main/java/classes/commands/Clear.java
@@ -2,6 +2,7 @@ package classes.commands;
 
 import classes.NamedCommand;
 import classes.collection.CollectionManager;
+import classes.console.TextColor;
 import interfaces.Commandable;
 
 public class Clear extends NamedCommand implements Commandable {
@@ -9,6 +10,7 @@ public class Clear extends NamedCommand implements Commandable {
     @Override
     public void execute(String... args) {
         new CollectionManager().clear();
+        System.out.println(TextColor.cyan("Коллекция был очищена"));
     }
 
     @Override
