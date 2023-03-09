9a61e2f0df14da055baf747b2cc62ffad4a55c14
Poolyakoffffffff <59867043+GreedlyCore@users.noreply.github.com>
2023-03-09 22:17:22 +0300

    Added remove_at_index command (#20)
    * add remove at index command
    
    * fix try-catch handler

---
+++ src/main/java/classes/commands/RemoveAtIndex.java
@@ -0,0 +1,33 @@
+package classes.commands;
+
+import classes.NamedCommand;
+import classes.collection.CollectionManager;
+import classes.console.TextColor;
+import interfaces.Commandable;
+
+public class RemoveAtIndex extends NamedCommand implements Commandable {
+
+    @Override
+    public String getInfo() {
+        return getName() + "\t-\tудалить элемент, находящийся в заданной позиции коллекции (index)";
+    }
+
+    @Override
+    public void execute(String... args) {
+        if (args.length == 1) {
+            try {
+                int idx = Integer.parseInt(args[0]);
+                new CollectionManager().getCollection().remove(idx);
+                System.out.println(TextColor.cyan("Элемент успешно удалён"));
+            } catch (NumberFormatException e) {
+                System.out.println(TextColor.yellow("Неверный формат ввода. Введите индекс в формате целочисленного числа через пробел"));
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println(TextColor.yellow("Введенный индекс выходит за пределы размера нынешней коллекции"));
+            }
+
+        } else {
+            System.out.println(TextColor.yellow("Неверный формат ввода. Введите индекс в формате целочисленного числа через пробел"));
+        }
+
+    }
+}
