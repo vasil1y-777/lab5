815d3c35ef265f2e8a7833b4d7b48faa20894a96
Poolyakoffffffff <59867043+GreedlyCore@users.noreply.github.com>
2023-03-09 21:52:52 +0300

    Update add command with arguments (#19)
    * add output for clear command
    
    * create add command
    
    * delete blank space

--- src/main/java/classes/commands/Clear.java
+++ src/main/java/classes/commands/Clear.java
@@ -2,13 +2,14 @@ package classes.commands;
 
 import classes.NamedCommand;
 import classes.collection.CollectionManager;
+import classes.console.TextColor;
 import interfaces.Commandable;
 
 public class Clear extends NamedCommand implements Commandable {
-
     @Override
     public void execute(String... args) {
         new CollectionManager().clear();
+        System.out.println(TextColor.cyan("Коллекция был очищена"));
     }
 
     @Override
