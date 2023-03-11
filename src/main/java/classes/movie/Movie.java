ae110958afc3fc1a0b4f8164ec20c0d3a11a554c
GreedlyCore <naganariselves@gmail.com>
2023-03-11 22:04:46 +0300

    update Movie class

--- src/main/java/classes/movie/Movie.java
+++ src/main/java/classes/movie/Movie.java
@@ -95,6 +95,10 @@ public class Movie {
         return creationDate;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     /**
      * Restrictions: Field should be unique & generating automatically
      *
