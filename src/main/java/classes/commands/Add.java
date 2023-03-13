6e9a7c5efd0766c0e1678ffcbf51b3c5e80aa490
GreedlyCore <naganariselves@gmail.com>
2023-03-13 22:15:13 +0300

    return add random command

--- src/main/java/classes/commands/Add.java
+++ src/main/java/classes/commands/Add.java
@@ -3,8 +3,12 @@ package classes.commands;
 import classes.NamedCommand;
 import classes.collection.CollectionManager;
 import classes.console.InputHandler;
+import classes.console.TextColor;
 import classes.movie.Movie;
+import classes.movie.RandomMovie;
 import interfaces.Commandable;
+
+import java.util.Objects;
 
 public class Add extends NamedCommand implements Commandable {
     @Override
@@ -14,9 +18,28 @@ public class Add extends NamedCommand implements Commandable {
 
     @Override
     public void execute(String... args) {
+
         CollectionManager collectionManager = new CollectionManager();
+        if (Objects.equals(args[0], "random")) {
+            if (args.length == 2) {
+                int m = Integer.parseInt(args[1]);
+                for (int i = 0; i < m; i++) {
+                    collectionManager.addMovie(RandomMovie.generate());
+                }
+            } else if (args.length == 1) {
+                collectionManager.addMovie(RandomMovie.generate());
+            } else {
+                System.out.println(TextColor.yellow("Неверное количество аргументов для этой команды"));
+            }
+
+        } else {
+            InputHandler inputHandler = new InputHandler();
+            Movie movie = inputHandler.readMovie();
+            collectionManager.addMovie(movie);
+        }
         InputHandler inputHandler = new InputHandler();
         Movie movie = inputHandler.readMovie();
         collectionManager.addMovie(movie);
     }
+
 }
