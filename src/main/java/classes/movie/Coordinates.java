ef0c126752e5fb452f2f6f72f2a13f6e92ff0a76
GreedlyCore <naganariselves@gmail.com>
2023-03-13 20:54:18 +0300

    update coordinates from unnecessary code

--- src/main/java/classes/movie/Coordinates.java
+++ src/main/java/classes/movie/Coordinates.java
@@ -11,13 +11,14 @@ import static classes.movie.FieldProperty.MAX_VALUE;
  * Model of Coordinates. Sub-model of the <code>Route</code>. Contains getters/setters of each class fields.
  * Some fields have restrictions. It's signed under every method of field.
  */
-public class Coordinates implements Comparable<Coordinates> {
+public class Coordinates {
     private long x; //Максимальное значение поля: 279
     private int y; //Значение поля должно быть больше -230
 
-    private Coordinates(){}
+    private Coordinates() {
+    }
 
-    public Coordinates(long x, int y)  throws NotGreatThanException, NullValueException, GreatThanException  {
+    public Coordinates(long x, int y) throws NotGreatThanException, NullValueException, GreatThanException {
         this.x = new FieldHandler(x, MAX_VALUE).handleLong();
         this.y = new FieldHandler(y, GREAT_THAN_X).handleInt();
     }
@@ -52,14 +53,5 @@ public class Coordinates implements Comparable<Coordinates> {
     public String toString() {
         return "(" + x + ", " + y + ")";
     }
-    @Override
-    public int compareTo(Coordinates o) {
-        if ((getX() == o.getX()) && (getY()==o.getY())){
-            return 0;
-        }else if (getX() != o.getX()){
-            return (int) (getX() - o.getX());
-        }else{
-            return getY() - o.getY();
-        }
-    }
+
 }
