afb550d29aeb8ae07286dc045dd791a04c2c1bdb
GreedlyCore <naganariselves@gmail.com>
2023-03-13 20:28:07 +0300

    update XML manager

--- src/main/java/classes/xml_manager/XMLMovieManager.java
+++ src/main/java/classes/xml_manager/XMLMovieManager.java
@@ -1,6 +1,7 @@
 package classes.xml_manager;
 
 import classes.DataStorage;
+import classes.console.TextColor;
 import classes.movie.Movies;
 
 import javax.xml.bind.JAXBContext;
@@ -15,13 +16,6 @@ import java.io.*;
  * Some fields have restrictions. It's signed under every method of field.
  */
 public class XMLMovieManager {
-    /*
-    TODO чтение xml файлов можно без Scanner ++
-    TODO запись в файлы реализовать с помощью PrintWriter ++
-    TODO добавить итераторы ??
-     */
-
-
     private static XMLMovieManager INSTANCE;
     private final Class BASE_CLASS = Movies.class;
 
@@ -37,7 +31,28 @@ public class XMLMovieManager {
         try {
             JAXBContext context = JAXBContext.newInstance(BASE_CLASS);
             Unmarshaller jaxbUnmarshaller = context.createUnmarshaller();
-            movies = (Movies) jaxbUnmarshaller.unmarshal(new File(DataStorage.STORAGE_FILE_PATH));
+            movies = (Movies) jaxbUnmarshaller.unmarshal(new File(DataStorage.getCurrentStorageFilePath()));
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        return movies;
+    }
+
+    public Movies readCollectionFromXML(String filepath) {
+        Movies movies = null;
+        File f = new File(filepath);
+        if (!(f.exists() && !f.isDirectory())) {
+            DataStorage.setCurrentStorageFilePath(DataStorage.DEFAULT_STORAGE_FILE_PATH);
+            System.out.println(TextColor.purple("Введённой коллекции не существует. Выбрана коллекция по-умолчанию"));
+        } else {
+            DataStorage.setCurrentStorageFilePath(filepath);
+            System.out.println(TextColor.purple("Коллекция найдена и была выбрана"));
+        }
+        File file = new File(DataStorage.getCurrentStorageFilePath());
+        try {
+            JAXBContext context = JAXBContext.newInstance(BASE_CLASS);
+            Unmarshaller jaxbUnmarshaller = context.createUnmarshaller();
+            movies = (Movies) jaxbUnmarshaller.unmarshal(file);
         } catch (JAXBException e) {
             e.printStackTrace();
         }
@@ -50,28 +65,23 @@ public class XMLMovieManager {
             jaxbContext = JAXBContext.newInstance(BASE_CLASS);
             Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
             jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-
-            File file = new File(DataStorage.STORAGE_FILE_PATH);
+            File file = new File(DataStorage.getCurrentStorageFilePath());
             file.createNewFile(); // checks existence of file and create if necessary
-            Movies mmm = new Movies();
+            Movies m = new Movies();
             StringWriter sw = new StringWriter();
             jaxbMarshaller.marshal(movies, sw);
-            String xmlString = sw.toString();
             sw.close();
-
-            PrintWriter writer = new PrintWriter(DataStorage.STORAGE_FILE_PATH);
+            PrintWriter writer = new PrintWriter(DataStorage.getCurrentStorageFilePath());
             writer.print(sw);
             writer.close();
-
-
         } catch (FileNotFoundException e) {
-            System.out.println("File not found error");
+            System.out.println(TextColor.grey("File not found error"));
             e.printStackTrace();
         } catch (IOException e) {
-            System.out.println("An IO error occurred");
+            System.out.println(TextColor.grey("An IO error occurred"));
             e.printStackTrace();
         } catch (JAXBException e) {
+            System.out.println(TextColor.grey("Ошибка чтения XML файла"));
             e.printStackTrace();
         }
     }
