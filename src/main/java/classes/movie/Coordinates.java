c46d6f28b87877fc161151b270ef30598208137f
GreedlyCore <naganariselves@gmail.com>
2023-03-13 20:55:09 +0300

    update coordinates

--- src/main/java/classes/movie/Coordinates.java
+++ src/main/java/classes/movie/Coordinates.java
@@ -11,7 +11,7 @@ import static classes.movie.FieldProperty.MAX_VALUE;
  * Model of Coordinates. Sub-model of the <code>Route</code>. Contains getters/setters of each class fields.
  * Some fields have restrictions. It's signed under every method of field.
  */
-public class Coordinates {
+public class Coordinates implements Comparable<Coordinates> {
     private long x; //Максимальное значение поля: 279
     private int y; //Значение поля должно быть больше -230
 
@@ -58,5 +58,16 @@ public class Coordinates {
     @Override
     public String toString() {
         return "(" + x + ", " + y + ")";
+    }
+
+    @Override
+    public int compareTo(Coordinates o) {
+        if ((getX() == o.getX()) && (getY()==o.getY())){
+            return 0;
+        }else if (getX() != o.getX()){
+            return (int) (getX() - o.getX());
+        }else{
+            return getY() - o.getY();
+        }
     }
 }
